7d84a94e2c6123e2091a7979791029f413b28379
Richard Hillman <Richard.CC.Hillman@gmail.com>
2020-10-17 21:05:46 -0700

    shit works

--- adventure-folder/adventure.js
+++ adventure-folder/adventure.js
@@ -7,18 +7,13 @@ import howGrumpy from './how-grumpy.js';
 
 // import howGrumpy from '../quests/how-grumpy';
 // import createAdventureLink from './create-adventure-link.js';
-import completedAll from './completedAll.js';
-
-// grab dom-----------------------------------------------------------------------------------------------
-// create a URL for my page i think? 
-// const myUrl = new URL('https://www.frogandtoadquests.dev');
+// import completedAll from './completedAll.js';
 
 // get the character and stick it at the top
 loadChar();
 
-// WHY DO I HAVE THIS HERE grabs my section so i can append it
-const section = document.getElementById('section');
 
+// grab dom-----------------------------------------------------------------------------------------------
 
 // get adventures query param
 const searchParameters = new URLSearchParams(window.location.search);
@@ -27,75 +22,81 @@ const adventureID = searchParameters.get('id');
 // find the adventure
 const adventure = findById(adventures, adventureID);
 
+const section = document.getElementById('section');
 const result = document.getElementById('result');
 const resultDescription = document.getElementById('result-description');
-const choiceForm = document.getElementById('choice-form');
+const description = document.getElementById('first');
+const form = document.getElementById('choice-form');
+const title = document.getElementById('title');
 
+
+// set state------------------------------------------------------------------------------------------------
 
 // make stuff-----------------------------------------------------------------------------------------------
+title.textContent = adventure.title;
+description.textContent = adventure.description;
 
-// make h3 element
-const h3 = document.createElement('h3');
-// take adventure title and put in h3 textcontent
-h3.textcontent = adventure.title;
-// appendchild 
-section.append(h3);
-
-// create a damn form
-const form = document.createElement('form');
-section.appendChild(form);
 // function createChoice(choices) {    
-// ----------------------------------------------------------------------------------------------------Problem CHild
-// const label = document.createElement('label');
-// const span = document.createElement('span');
-
+    // ----------------------------------------------------------------------------------------------------Problem CHild
 for (let index = 0; index < adventure.choices.length; index++) {
     const choice = adventure.choices[index];
     const choiceDOM = choiceCreator(choice);
     form.appendChild(choiceDOM);
 }
 
-    // console.log(adventures[0].choices[0].description);
-    // span.textContent = adventures.description;
-    
-    // const radio = document.createElement('input');
-    // radio.type = 'radio';
-    // radio.name = 'choices';
-    // radio.value = choice.id;
-    // radio.required = true;
-    
+// section.appendChild(form);
+// for each of the quest's choices
+
+
+
+// const radio = document.createElement('input');
+// radio.type = 'radio';
+// radio.name = 'choices';
+// radio.value = choice.id;
+// radio.required = true;
+
 // label.append(span, radio);
 // form.appendchild(label);
 // ----------------------------------------------------------------------------------------------------------------
-
-
 // create my self a button for pressin
+
+// create a damn form
+
+// description.textContent = adventures[0].description;
+
+// const h3 = document.createElement('h3');
+// h3.textcontent = adventures.title;
+// section.append(h3);
+
 const button = document.createElement('button');
-// call that button for pressin next
-button.textContent = 'NEXT';
+button.textContent = 'Decision';
 form.appendChild(button);
-
-// because i have a form I have to have submit 
-// now i also have to decide what to do when i click this submit button
-// so far i have checked so i can grab the users selection
-// I probably want to log that user selection and put into local storage and update my dom for my character
-
+// -------------------------------------------------------------------------------Next Button
+// const button2 = document.createElement('button');
+// button.textContent = 'Next';
+// form.appendChild(button);
+// -------------------------------------------------------------------------------
+    // because i have a form I have to have submit 
+    // now i also have to decide what to do when i click this submit button
+    // so far i have checked so i can grab the users selection
+    // I probably want to log that user selection and put into local storage and update my dom for my character
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    
-    const formData = new FormData(choiceForm);
+        
+    const formData = new FormData(form);
     const choiceId = formData.get('choice');
-
+    
     const choice = findById(adventure.choices, choiceId);
-
+    
     const user = getUser();
     howGrumpy(choice, adventure.id, user);
     saveUser(user);
     
-    choiceForm.classList.add('hidden');
+    form.classList.add('hidden');
     result.classList.remove('hidden');
-    resultDescription.textContent = choice.result
-
+    resultDescription.textContent = choice.result;
+});
+    
     // const checked = document.querySelectorAll(':checked');
     // const userChecked = checked.value;
     // const choice = findById(adventure.choices, userChecked);
@@ -104,14 +105,13 @@ form.addEventListener('submit', (e) => {
 
     // user.grumpyLevel += choice.grumpyLevel;
     // user.buttons += choice.buttons;
-
+    
     // user.completed.id = true;
 
     // saveUser(user);
- 
+    
     // if (user.grumpyLevel >= 10 || completedAll(adventures, user)) {
-    //     window.location.href = '../grumpyHouse';
-    // }
-});
+        //     window.location.href = '../grumpyHouse';
+        // }
 
 // append that shit
